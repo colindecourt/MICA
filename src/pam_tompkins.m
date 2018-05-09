f21eb6824167f0400fe721ee17a55855983dc9c6
Unknown <lamiae.elamri@yahoo.com>
2018-05-09 18:03:43 +0200

    debut

---
+++ src/pam_tompkins.m
@@ -0,0 +1,10 @@
+clear all; 
+close all;
+clc;
+
+%% Load a signal
+[file,path] = uigetfile('*.mat', 'rt');
+signal = load(fullfile(path, file));
+data = signal.ecg; % Your ecg data
+Fs = signal.Fs; % Sampling frequency
+N = size(data,2); % Data length
