d402a2c7586049748b638a1b454689f97460edeb
Decourt <decourtcolin@gmail.com>
2018-05-16 18:27:19 +0200

    QRS detection

--- src/pam_tompkins.m
+++ src/pam_tompkins.m
@@ -1,11 +1,11 @@
-clear all; 
+clear all;
 close all;
 clc;
 
 %% Load a signal
 
 
-signal = load('ecg_noiseBL.mat');
+signal = load('ecg_normal_1.mat');
 data = signal.ecg;
 Fs = signal.Fs; % Sampling frequency
 
@@ -13,18 +13,18 @@ figure
 plot(data);
 
 
-%% Band-pass filter 
+%% Band-pass filter
 
-% Low-pass filter 
+% Low-pass filter
 
 b1=[1 0 0 0 0 0 -2 0 0 0 0 0 1];
 a1=[1 -2 1];
 
-Y1 = filter(b1, a1, data); 
-% fvtool(b1, a1); %enables to see the delay introduced by the first filter 
+Y1 = filter(b1, a1, data);
+% fvtool(b1, a1); %enables to see the delay introduced by the first filter
 
 
-% High-pass filter 
+% High-pass filter
 
 b2=zeros(1, 33);
 b2(1)=-1;
@@ -33,7 +33,7 @@ b2(18)=-32;
 b2(33)=1;
 a2=[1 -1];
 
-Y2=filter(b2,a2,Y1); % signal filtered 
+Y2=filter(b2,a2,Y1); % signal filtered
 % fvtool(b2, a2); %enables to see the delay introduced by the second filter
 
 
@@ -41,13 +41,13 @@ Y2=filter(b2,a2,Y1); % signal filtered
 % plot(Y2(1:500));
 
 
-%plot(Y2(1:500)/max(abs(Y2(1:500)))); % normalized signal 
+%plot(Y2(1:500)/max(abs(Y2(1:500)))); % normalized signal
 
 
-%% Differenciation 
+%% Differenciation
 
 b3=(Fs/8).*[1 2 0 -2 -1];
-a3=[1]; % the delay of 2 samples is for now ignored 
+a3=[1]; % the delay of 2 samples is for now ignored
 
 Y3=filter(b3, a3, Y2);
 
@@ -74,9 +74,9 @@ for i=1:400:length(data)-400
     R = max(Smwi(i:i+400)); % Search the max during a period of 200 samples
     m = mean(Smwi(i:i+400)); %Search the mean to don't detect noise peak (as P wave for example)
     for k=i:i+400
-     A(k) = 0.875*R+0.125*PEAK; 
-     
-     M(k) = 0.875*m+0.125*PEAK;
+        A(k) = 0.875*R+0.125*PEAK;
+        
+        M(k) = 0.875*m+0.125*PEAK;
     end
 end
 
@@ -90,11 +90,11 @@ TRESH2 = 0.5*TRESH1;
 %plot(TRESH2(1:500)/max(abs(TRESH2(1:500))), 'green'); % normalize tresh
 
 
-delay=38 % delay in samples 
+delay=38 % delay in samples
 
 
 
-%% Detection of maxima 
+%% Detection of maxima
 
 %Detection of the QRS location
 T=zeros(size(Smwi));
@@ -115,7 +115,7 @@ for k=1:length(T1)
 end
 index=index(2:end)-delay; %index of QRS location
 
-%R detection 
+%R detection
 R_peak=[0];
 R_peak_abs=[0];
 
@@ -131,10 +131,38 @@ for j=1:length(R_peak)
 end
 R_peak_abs = R_peak_abs(2:end);
 hold on
-plot(R_peak_abs,R_peak,'*');
-    
+plot(R_peak_abs,R_peak,'o');
 
 
+%Q detection
+Q_peak_abs=[0];
+c=R_peak_abs(1);
+for i=2:length(R_peak_abs)
+    while(data(c)>data(c-1))
+        c=c-1;     
+    end
+    Q_peak_abs=[Q_peak_abs,c];
+    c=R_peak_abs(i);
+end
+Q_peak_abs=Q_peak_abs(2:end);
 
+Q_peak=data(Q_peak_abs);
 
+plot(Q_peak_abs,Q_peak,'o');
 
+%S detection
+S_peak_abs=[0];
+
+d=R_peak_abs(1);
+for i=2:length(R_peak_abs)
+    while(data(d)>data(d+1))
+        d=d+1;     
+    end
+    S_peak_abs=[S_peak_abs,d];
+    d=R_peak_abs(i);
+end
+S_peak_abs=S_peak_abs(2:end);
+
+S_peak=data(S_peak_abs);
+
+plot(S_peak_abs,S_peak,'o');
